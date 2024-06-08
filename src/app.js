c4a28887a7a8d2a2f6988e72051881a94ecc107f
keyaankie <151882832+keyaankie@users.noreply.github.com>
2024-06-09 01:11:29 +0200

    injected more weather data

--- src/app.js
+++ src/app.js
@@ -4,7 +4,43 @@ function updateWeather(response){
   temperatureElement.innerHTML = currentTemperature; 
   let cityElement = document.querySelector("#city"); 
    cityElement.innerHTML = response.data.city;
+          
+     
+ let descriptionElement = document.querySelector("#description");
+ let humidityElement = document.querySelector("#humidity");
+ let windElement = document.querySelector("#wind");
+ let timeElement = document.querySelector("#time");
+ let date = new Date(response.data.time * 1000);
 
+ currentDescription = response.data.condition.description;
+ currentHumidity = response.data.temperature.humidity;
+ currentWindSpeed = response.data.wind.speed;
+
+ descriptionElement.innerHTML = currentDescription;
+ humidityElement.innerHTML =  `${currentHumidity}%`;
+ windElement.innerHTML = `${currentWindSpeed}km/h`;
+ timeElement.innerHTML =  formatDate(date);
+
+  
+
+
+
+
+
+}
+function formatDate(date){
+ 
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+  let day = days[date.getDay()];
+  if (hours < 10){
+    hours = `0${hours}`;
+  }
+  if (minutes < 10){
+    minutes = `0${minutes}`;
+  }
+  return `${day}, ${hours}:${minutes}`;
 }
 
 
@@ -26,3 +62,4 @@ let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Pretoria")
+ 
