74a732067399985eb7a341ba360de23e42a92096
keyaankie <151882832+keyaankie@users.noreply.github.com>
2024-06-25 00:47:00 +0200

    Injected HTML from JavaScript

--- src/app.js
+++ src/app.js
@@ -24,11 +24,6 @@ function updateWeather(response){
  windElement.innerHTML = `${currentWindSpeed}km/h`;
  timeElement.innerHTML =  formatDate(date);
 
-  
-
-
-
-
 
 }
 function formatDate(date){
@@ -60,9 +55,34 @@ function handleSearchSubmit(event) {
  
   searchCity(searchInput.value);
 }
+function displayForecast() {
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+      <div class="weather-forecast-day">
+        <div class="weather-forecast-date">${day}</div>
+        <div class="weather-forecast-icon">🌤️</div>
+        <div class="weather-forecast-temperatures">
+         <div class="weather-forecast-temperature">
+            <strong>15º</strong>
+          </div>
+          <div class="weather-forecast-temperature-min">9º</div>
+          
+        </div>
+      </div>
+    `;
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml; }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Pretoria")
+ displayForecast();
  
