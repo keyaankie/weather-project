80652cf6ad0d428cbbb3a0b9b043305648d2994b
keyaankie <151882832+keyaankie@users.noreply.github.com>
2024-06-25 03:22:42 +0200

    added API integration

--- src/app.js
+++ src/app.js
@@ -55,6 +55,13 @@ function handleSearchSubmit(event) {
  
   searchCity(searchInput.value);
 }
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
 function getForecast(city) {
   let apiKey = "22afdao0cbf834te6b26411a631e5b10";
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
@@ -66,22 +73,23 @@ function displayForecast(response) {
   let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
   let forecastHtml = "";
 
-  days.forEach(function (day) {
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5){
     forecastHtml =
       forecastHtml +
       `
       <div class="weather-forecast-day">
-        <div class="weather-forecast-date">${day}</div>
-        <div class="weather-forecast-icon">🌤️</div>
+        <div class="weather-forecast-date">${formatDay(day.time)}</div></div>
+        <div class="weather-forecast-icon"><img src="${day.condition.icon_url}"</div>
         <div class="weather-forecast-temperatures">
          <div class="weather-forecast-temperature">
-            <strong>15º</strong>
+            <strong>${Math.round(day.temperature.maximum)}°</strong>
           </div>
-          <div class="weather-forecast-temperature-min">9º</div>
+          <div class="weather-forecast-temperature-min">${Math.round(day.temperature.minimum)}°</div>
           
         </div>
       </div>
-    `;
+    `;}
   });
 
   let forecastElement = document.querySelector("#forecast");
